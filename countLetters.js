dc707a5ad5cc5c4ce1a5c0af88aaadb8cb1a36f1
Anthony Anuebunwa <anuebunwaanthony1@gmail.com>
2024-06-17 16:34:22 -0400

    Added more test cases

--- countLetters.js
+++ countLetters.js
@@ -9,7 +9,7 @@ const assertEqual = function(actual, expected) {
 /**@param {string} word - the string to be passed through the function.*/
 /**@returns {object} results - Letters in word with their frequency.*/
 const countLetters = function(word) {
-  let rmSpaceFromWord = word.replace(' ', '');
+  let rmSpaceFromWord = word.replace(/ /g, '');
   const results = {};
 
   for (let letter of rmSpaceFromWord) {
@@ -26,15 +26,20 @@ const countLetters = function(word) {
 
 let sauce1 = countLetters("Szechuan Sauce");
 let sauce2 = countLetters("Worcestershire sauce");
+let sauce3 = countLetters("Hot Chipotle Sauce")
 
 console.log(`Sauce-1 Keys: ${Object.keys(sauce1).join('|')}`);
 console.log(`Sauce-1 Vals: ${Object.values(sauce1).join('|')}\n\n`);
 console.log(`Sauce-2 Keys: ${Object.keys(sauce2).join('|')}`);
-console.log(`Sauce-2 Vals: ${Object.values(sauce2).join('|')}\n\n`);
+console.log(`Sauce-2 Keys: ${Object.values(sauce2).join('|')}\n\n`);
+console.log(`Sauce-3 Keys: ${Object.keys(sauce3).join('|')}`);
+console.log(`Sauce-3 Vals: ${Object.values(sauce3).join('|')}\n\n`);
 
 assertEqual(sauce1['S'], 2);
 assertEqual(sauce1['z'], 1);
 assertEqual(sauce1['n'], 1);
 assertEqual(sauce2['S'], undefined);
 assertEqual(sauce2[' '],undefined);
-assertEqual(sauce1[' '], undefined);
+assertEqual(sauce3[' '], undefined);
+assertEqual(sauce3['H'], 1);
+assertEqual(sauce3['h'], 1);
