a20d59ca024246d545b84ca7b9926f4d3ef87a7b
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-07-01 18:54:32 -0400

    more

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,4 +1,5 @@
 const assertArraysEqual = require ("../assertArraysEqual");
+const assertEqual = require ("../assertEqual")
 const eqArrays = require ("../eqArrays");
 
 assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
