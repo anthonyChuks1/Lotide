fa54c370a6c42fac87f7f6bdfbbff327bf19351d
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-07-01 19:26:16 -0400

    mocha ad chai implement 1

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,18 @@
 const middle = require("../middle")
 const assertArraysEqual = require("../assertArraysEqual")
+const assert = require("chai").assert;
 
-assertArraysEqual(middle([2, 3, 5, 6]), [3, 5]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Even length
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Even length
-assertArraysEqual(middle([1, 2, 3]), [2]); // Odd length
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Odd length
-assertArraysEqual(middle([1]), []); // Single element
-assertArraysEqual(middle([1, 2]), []); // Two elements
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]); // 8 indexes Even length
-assertArraysEqual(middle([1, 2, 5, 7, 5, 6]), [5, 7]); //  6 Indexes Even length
+describe ("#middle", () => {
+  it("([2, 3, 5, 6]) returns  [3, 5]", () => {
+    assert.deepEqual(middle([2, 3, 5, 6]), [3, 5]);
+  })
+  it("([1, 2, 3, 4]) returns  [2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  })
+  it("([1,2]) returns  []", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  })
+  it("([1, 2, 3, 4, 5, 6, 7, 8]) returns  [4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  })
+}) 
