59938d0965197bc8988d669828c69e11643ac682
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-07-01 18:14:37 -0400

    test

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,6 @@
-assertEqual = require("../assertEqual")
-head = require("../head")
+ const assertEqual = require("../assertEqual")
+ const head = require("../head")
+ 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5]), 5);
